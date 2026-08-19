375f8e5957d569e9fb2e8afad9c750269902bb49
Nguyen <vinhomes1910999@gmail.com>
2026-08-19 15:36:56 +0700

    Doi link admin bao mat

--- ecommerce_config/urls.py
+++ ecommerce_config/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('god/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('cart/', include('cart.urls')),
     path('orders/', include('orders.urls')),
